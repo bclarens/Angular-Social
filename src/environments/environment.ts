75cf833d1a9db73185fc27612d462d6a89e8b2f2
Clarence Anne <burgosclarens@gmail.com>
2018-03-14 15:14:11 +0800

    Removed unnecessary functions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyCBqvGswPEmi5986od8iS6HQIFLmI833vQ",
+    authDomain: "postnote-bac57.firebaseapp.com",
+    databaseURL: "https://postnote-bac57.firebaseio.com",
+    projectId: "postnote-bac57",
+    storageBucket: "postnote-bac57.appspot.com",
+    messagingSenderId: "633673181770"
     }
 };
